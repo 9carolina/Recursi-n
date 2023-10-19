07e803cf7351110b948edb2cc1084cc9b2037128
9carolina <141955867+9carolina@users.noreply.github.com>
2023-10-19 12:30:59 -0600

    Add files via upload

---
+++ Recursión1. Funciones recursivas.c
@@ -0,0 +1,16 @@
+//Recursión
+#include<stdio.h>
+int factorial(int num){
+	if (num==0)
+		return 1;
+	else
+		return (factorial(num-1)*num); //(num-1) es como un i++, es nuestro incrementador
+}
+int main(){
+	int numero;
+	printf("Ingresa un numero para calcular su factorial: ");
+	scanf("%d", &numero);
+	printf("\n");
+	printf("El factorial de %d es %d\n", numero, factorial(numero));
+	return 0;
+}
